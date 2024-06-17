c0c67ae407f14a6d5400381660af1971c00ce21f
Iwanaga-Sena <171421410+Iwanaga-Sena@users.noreply.github.com>
2024-06-17 19:14:42 +0900

    [fix] routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'books' => 'books#index_and_new'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  delete 'books/:id' => 'books#destroy', as: 'book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
