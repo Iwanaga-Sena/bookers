9b0005681d700c526a5c35d8cbf2cf402ad30e57
EC2 Default User <ec2-user@ip-172-31-8-207.ap-northeast-1.compute.internal>
2024-06-17 11:10:30 +0000

    [fix] routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
-  def index_and_new
+  def index
     @books = Book.all
+    @book = Book.new
+  end
+  
+  def new
     @book = Book.new
   end
 
@@ -19,9 +23,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def destroy
