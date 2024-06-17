ae4a8a0da9183b630b7542219c5e0f756c2a8e25
Iwanaga-Sena <171421410+Iwanaga-Sena@users.noreply.github.com>
2024-06-17 18:58:43 +0900

    [add] destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   patch 'books/:id' => 'books#update', as: 'update_book'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
